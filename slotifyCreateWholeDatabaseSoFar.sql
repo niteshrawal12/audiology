dbea27020a814679c4f43e3b5cd8dd07fe270cd9
Nitesh Rawal <66133661+niteshrawal12@users.noreply.github.com>
2021-03-20 00:58:23 +0530

    Update slotifyCreateWholeDatabaseSoFar.sql

--- slotifyCreateWholeDatabaseSoFar.sql
+++ slotifyCreateWholeDatabaseSoFar.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 27, 2017 at 10:12 AM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
